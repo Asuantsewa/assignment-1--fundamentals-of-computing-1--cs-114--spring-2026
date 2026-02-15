0c7c5f62ab3fd40dbc8b72146aa7100653d1eab5
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2026-02-14 21:25:01 -0500

    finished the first part of program 1

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Assignment1Program1 {
   public static void main(String[] args) {
-
-  }
+      System.out.println("JJJJJ      EEEEE");
+      System.out.println("  J        E    ");
+      System.out.println("  J        EEEE ");
+      System.out.println("J J        E    ");
+      System.out.println(" J         EEEEE");
+    }
+      Scanner scan = new Scanner(System.in);
+    System.out.println(" Enter a 5 string character: ");
 }
