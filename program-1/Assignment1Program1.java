bc25759b100a7a6b56d1e8d93ef700fcc3748bbf
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2026-02-17 16:02:34 -0500

    Writing the code  for the string

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -7,8 +7,18 @@ public class Assignment1Program1 {
       System.out.println("  J        EEEE ");
       System.out.println("J J        E    ");
       System.out.println(" J         EEEEE");
+
       Scanner input = new Scanner(System.in);
       System.out.print("Enter a 5 chracter string: ");
+      String inpuString = input.nextLine();
+      if (input.length() != 5) {
+        System.out.println("Invalid input. Please enter exactly 5 characters. ");
+        return;
+
+         String middleString = input.substring(1,4);
+         String reversedmiddleString = new String(middleString). reversedMiddle().toString();
+      }
+
 
       input.close();
   }
