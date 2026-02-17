a210e2a1f1601651f82f961fedb96eceed16d6cd
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2026-02-17 15:53:24 -0500

    solved error

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -7,9 +7,9 @@ public class Assignment1Program1 {
       System.out.println("  J        EEEE ");
       System.out.println("J J        E    ");
       System.out.println(" J         EEEEE");
-      String input;
       Scanner input = new Scanner(System.in);
       System.out.print("Enter a 5 chracter string: ");
-      String input = scanner.nextLine();
+
+      input.close();
   }
 }
