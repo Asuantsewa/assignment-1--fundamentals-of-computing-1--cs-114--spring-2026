32df4d433349384d36aaf10172939d772b07ba72
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2026-02-17 21:44:30 -0500

    program code

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -8,7 +8,7 @@ public class Assignment1Program1 {
       System.out.println("J J        E    ");
       System.out.println(" J         EEEEE");
 
-      Scanner input = new Scanner(System.in);
+      /*Scanner input = new Scanner(System.in);
       System.out.print("Enter a 5 chracter string: ");
       /*String inpuString = input.nextLine();
       if (input.length() != 5) {
@@ -17,10 +17,14 @@ public class Assignment1Program1 {
          String middleString = input.substring(1,4);
          String reversedmiddleString = new String(middleString). reversedMiddle().toString();
       }*/
-     double farenheit;
-
-    System.out.println("Enter a nimber in Farenheit: ");
-     farenheit = input.nextLine(); 
+      double farenheitTemp;
+     Scanner temp = new Scanner(System.in);
+     System.out.println("Enter a nimber in Farenheit: ");
+     farenheitTemp = temp.nextDouble();
+      double celsuis = (farenheitTemp - 32) / 1.98;
+      int intcelsuis = (int) celsuis;
+      System.out.println(intcelsuis);
+      
       
      input.close();
   }
