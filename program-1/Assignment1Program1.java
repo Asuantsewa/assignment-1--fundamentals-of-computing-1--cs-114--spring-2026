b9deb1d1150f02c5f49e66dd996de54d0745ab78
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2026-02-22 17:44:22 -0500

    removed comments and added codes

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -3,21 +3,25 @@ import java.util.Scanner;
 public class Assignment1Program1 {
   public static void main(String[] args) {
       System.out.println("JJJJJ      EEEEE");
-      System.out.println("  J        E    ");
-      System.out.println("  J        EEEE ");
-      System.out.println("J J        E    ");
-      System.out.println(" J         EEEEE");
+      System.out.println("  JJ        E    ");
+      System.out.println("  JJ        EEEE ");
+      System.out.println("J JJ        E    ");
+      System.out.println("JJJ         EEEEE");
 
-      /*Scanner input = new Scanner(System.in);
+      Scanner scanner = new Scanner(System.in);
+      String input = scanner.nextLine();
       System.out.print("Enter a 5 chracter string: ");
-      /*String inpuString = input.nextLine();
       if (input.length() != 5) {
         System.out.println("Invalid input. Please enter exactly 5 characters. ");
-        return;
-         String middleString = input.substring(1,4);
-         String reversedmiddleString = new String(middleString). reversedMiddle().toString();
-      }*/
-      double farenheitTemp;
+      return;
+    }
+          
+      String middle = input.substring(1,4);
+        String reversedMiddle = new StringBuilder(middle).reverse().toString();
+        System.out.println( reversedMiddle);
+        scanner.close();
+      }
+     double farenheitTemp;
      Scanner temp = new Scanner(System.in);
      System.out.println("Enter a nimber in Farenheit: ");
      farenheitTemp = temp.nextDouble();
@@ -29,4 +33,3 @@ public class Assignment1Program1 {
      /*input.close();*/
      temp.close();
   }
-}
--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class Assignment1Program2 {
   public static void main(String[] args) {
+    Scanner baseInput = new Scanner(System.in);
+    
+
 
   }
 }
