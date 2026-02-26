744ffd45543f26cefcfd817bb150620af537dffc
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2026-02-26 16:47:56 -0500

    dne

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -31,6 +31,7 @@ public class Assignment1Program1 {
   Random number = new Random();
   int randNumber = number.nextInt(max - min + 1) + min;
   System.out.println("Random number generated: " + randNumber);
+    }
   }
   System.out.println( "Your new string is" +celsuis +reversedMiddle +randNumber);
 }
