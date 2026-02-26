ba59f7420e04cf7f5763df93e04a08ab90df1495
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2026-02-26 12:35:23 -0500

    printing the concated sentence

--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;
 public class Assignment1Program2 {
   public static void main(String[] args) {
-    Scanner baseInput = new Scanner(System.in);
-    
+    Scanner scanner = new Scanner(System.in);
+    int base = 10;
+    System.out.print(" Enter a base 10 numer to convert between (2 - 9)");
+    int baseNumber = scanner.nextInt();
+
+    if (baseNumber < 2 || baseNumber> 36) {
+      System.out.println("Invalid base. Please enter a base between 2 and 36.");
+    return;
+  }
+  System.out.println("The maximum, 4 digit, " + base +"number in base " + baseNumber + "is"); 
 
 
   }
